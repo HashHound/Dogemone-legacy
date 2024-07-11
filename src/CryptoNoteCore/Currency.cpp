7da3b5716d02ccea9668d0239c56e73bd9db31a5
RealEther <aravindkishore21@gmail.com>
2024-07-11 21:41:05 +0530

    Update Currency.cpp

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -139,7 +139,7 @@ bool Currency::constructGenesisTransaction(Transaction& tx) const {
   tx.outputs.push_back(out);
 
   tx.version = CURRENT_TRANSACTION_VERSION;
-  tx.unlockTime = 0;
+  tx.unlockTime = 60;
 
   return true;
 }
